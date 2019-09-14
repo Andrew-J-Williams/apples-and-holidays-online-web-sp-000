905175c3c9b941b69765beaed8e2697da7bd84c4
Andrew Williams <1williamsaj@gmail.com>
2019-09-14 01:48:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -93,10 +93,13 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  bbq_holidays = []
+
   holiday_hash.each do |key, value|
     value.each do |attribute, data|
       data.each do |items|
         if items == "BBQ"
+
 
     end
   end
